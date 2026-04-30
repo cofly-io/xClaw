1d7db93f04ecaf31ba638ea42c1e5bd860384ee8
hongxicheng <1003394729@qq.com>
2026-04-30 15:19:10 +0800

    feat(WeCom): add share_session_in_group toggle for group chats (#3948)

--- console/src/pages/Control/Channels/components/ChannelDrawer.tsx
+++ console/src/pages/Control/Channels/components/ChannelDrawer.tsx
@@ -826,6 +826,14 @@ export function ChannelDrawer({
             >
               <Input placeholder={t("channels.welcomeTextPlaceholder")} />
             </Form.Item>
+            <Form.Item
+              name="share_session_in_group"
+              label={t("channels.onebotShareSessionInGroup")}
+              valuePropName="checked"
+              tooltip={t("channels.onebotShareSessionInGroupTooltip")}
+            >
+              <Switch />
+            </Form.Item>
           </>
         );
 
