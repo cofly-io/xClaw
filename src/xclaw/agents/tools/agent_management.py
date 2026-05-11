devjotaduo/orbe

fix(cli): bypass proxies for loopback API checks (#4092)
Co-authored-by: Jinglin Peng <jinglinpeng@gmail.com>

src/qwenpaw/agents/tools/agent_management.py
@@ -13,6 +13,7 @@ from agentscope.message import TextBlock
 from agentscope.tool import ToolResponse
 
 from ...config.utils import read_last_api
+from ...utils.http import trust_env_for_url
 
 
 DEFAULT_AGENT_API_BASE_URL = "http://127.0.0.1:8088"
@@ -65,9 +66,11 @@ def create_agent_api_client(
     default_timeout: float = DEFAULT_AGENT_API_TIMEOUT,
 ) -> httpx.Client:
     """Create an HTTP client targeting the local agent API."""
+    normalized = _normalize_api_base_url(base_url)
     return httpx.Client(
-        base_url=_normalize_api_base_url(base_url),
+        base_url=normalized,
         timeout=default_timeout,
+        trust_env=trust_env_for_url(normalized),
     )
 
 

tests/unit/cli/test_cli_http_proxy.py
@@ -1,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from qwenpaw.agents.tools import agent_management
+from qwenpaw.cli import http as cli_http
+
+
+# CLI API clients hit the local QwenPaw service, so loopback URLs skip proxies.
+def test_cli_http_client_bypasses_env_for_loopback(monkeypatch) -> None:
+    captured: dict[str, object] = {}
+
+    def _fake_client(**kwargs):
+        captured.update(kwargs)
+        return object()
+
+    monkeypatch.setattr(cli_http.httpx, "Client", _fake_client)
+
+    cli_http.client("http://127.1.2.3:8088")
+
+    assert captured["trust_env"] is False
+
+
+# Explicit remote API targets should still honor the user's proxy env.
+def test_cli_http_client_keeps_env_for_remote_base_url(monkeypatch) -> None:
+    captured: dict[str, object] = {}
+
+    def _fake_client(**kwargs):
+        captured.update(kwargs)
+        return object()
+
+    monkeypatch.setattr(cli_http.httpx, "Client", _fake_client)
+
+    cli_http.client("http://192.168.1.10:8088")
+
+    assert captured["trust_env"] is True
+
+
+# Agent-management API helpers share the same local API proxy policy.
+def test_agent_api_client_bypasses_env_for_loopback(monkeypatch) -> None:
+    captured: dict[str, object] = {}
+
+    def _fake_client(**kwargs):
+        captured.update(kwargs)
+        return object()
+
+    monkeypatch.setattr(agent_management.httpx, "Client", _fake_client)
+
+    agent_management.create_agent_api_client("http://localhost:8088")
+
+    assert captured["trust_env"] is False

tests/unit/utils/test_http.py
@@ -1,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import pytest
+
+from qwenpaw.utils.http import is_loopback_url, trust_env_for_url
+
+
+# Local API calls should bypass env proxies for localhost/loopback targets.
+@pytest.mark.parametrize(
+    "url",
+    [
+        "http://localhost:8088/api",
+        "http://localhost.:8088/api",
+        "http://127.0.0.1:8088/api",
+        "http://127.1.2.3:8088/api",
+        "http://[::1]:8088/api",
+    ],
+)
+def test_is_loopback_url_recognizes_loopback_targets(url: str) -> None:
+    assert is_loopback_url(url) is True
+    assert trust_env_for_url(url) is False
+
+
+# Non-loopback URLs keep httpx's normal env proxy behavior.
+@pytest.mark.parametrize(
+    "url",
+    [
+        "http://192.168.1.10:8088/api",
+        "https://example.com/api",
+        "http://10.0.0.1:8088/api",
+    ],
+)
+def test_is_loopback_url_keeps_non_loopback_targets(url: str) -> None:
+    assert is_loopback_url(url) is False
+    assert trust_env_for_url(url) is True

cofly-io/xClaw

src/xclaw/agents/tools/agent_management.py
@@ -13,6 +13,7 @@ from agentscope.message import TextBlock
 from agentscope.tool import ToolResponse
 
 from ...config.utils import read_last_api
+from ...utils.http import trust_env_for_url
 
 logger = logging.getLogger(__name__)
 
@@ -27,7 +28,11 @@ def create_agent_api_client(
     base = (base_url or "").rstrip("/")
     if not base.endswith("/api"):
         base = f"{base}/api"
-    return httpx.Client(base_url=base, timeout=timeout)
+    return httpx.Client(
+        base_url=base,
+        timeout=timeout,
+        trust_env=trust_env_for_url(base),
+    )
 
 
 def resolve_agent_api_base_url() -> str:

tests/unit/cli/test_cli_http_proxy.py
@@ -1,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from xclaw.agents.tools import agent_management
+from xclaw.cli import http as cli_http
+
+
+# CLI API clients hit the local QwenPaw service, so loopback URLs skip proxies.
+def test_cli_http_client_bypasses_env_for_loopback(monkeypatch) -> None:
+    captured: dict[str, object] = {}
+
+    def _fake_client(**kwargs):
+        captured.update(kwargs)
+        return object()
+
+    monkeypatch.setattr(cli_http.httpx, "Client", _fake_client)
+
+    cli_http.client("http://127.1.2.3:8088")
+
+    assert captured["trust_env"] is False
+
+
+# Explicit remote API targets should still honor the user's proxy env.
+def test_cli_http_client_keeps_env_for_remote_base_url(monkeypatch) -> None:
+    captured: dict[str, object] = {}
+
+    def _fake_client(**kwargs):
+        captured.update(kwargs)
+        return object()
+
+    monkeypatch.setattr(cli_http.httpx, "Client", _fake_client)
+
+    cli_http.client("http://192.168.1.10:8088")
+
+    assert captured["trust_env"] is True
+
+
+# Agent-management API helpers share the same local API proxy policy.
+def test_agent_api_client_bypasses_env_for_loopback(monkeypatch) -> None:
+    captured: dict[str, object] = {}
+
+    def _fake_client(**kwargs):
+        captured.update(kwargs)
+        return object()
+
+    monkeypatch.setattr(agent_management.httpx, "Client", _fake_client)
+
+    agent_management.create_agent_api_client("http://localhost:8088")
+
+    assert captured["trust_env"] is False

tests/unit/utils/test_http.py
@@ -1,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import pytest
+
+from xclaw.utils.http import is_loopback_url, trust_env_for_url
+
+
+# Local API calls should bypass env proxies for localhost/loopback targets.
+@pytest.mark.parametrize(
+    "url",
+    [
+        "http://localhost:8088/api",
+        "http://localhost.:8088/api",
+        "http://127.0.0.1:8088/api",
+        "http://127.1.2.3:8088/api",
+        "http://[::1]:8088/api",
+    ],
+)
+def test_is_loopback_url_recognizes_loopback_targets(url: str) -> None:
+    assert is_loopback_url(url) is True
+    assert trust_env_for_url(url) is False
+
+
+# Non-loopback URLs keep httpx's normal env proxy behavior.
+@pytest.mark.parametrize(
+    "url",
+    [
+        "http://192.168.1.10:8088/api",
+        "https://example.com/api",
+        "http://10.0.0.1:8088/api",
+    ],
+)
+def test_is_loopback_url_keeps_non_loopback_targets(url: str) -> None:
+    assert is_loopback_url(url) is False
+    assert trust_env_for_url(url) is True