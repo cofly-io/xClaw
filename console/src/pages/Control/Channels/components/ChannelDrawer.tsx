devjotaduo/orbe

refactor(console): extract QrcodeAuthBlock component and fix polling leak on drawer close (#4153)

console/src/pages/Control/Channels/components/ChannelDrawer.tsx
@@ -8,15 +8,13 @@ import {
   Select,
 } from "@agentscope-ai/design";
 import { useAppMessage } from "../../../../hooks/useAppMessage";
-import { Alert, ConfigProvider, Spin } from "antd";
+import { Alert, ConfigProvider } from "antd";
 import { LinkOutlined } from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
 import type { FormInstance } from "antd";
-import { useCallback, useEffect } from "react";
 import { getChannelLabel, type ChannelKey } from "./constants";
-import { useChannelQrcode } from "./useChannelQrcode";
+import { QrcodeAuthBlock } from "./QrcodeAuthBlock";
 import styles from "../index.module.less";
-import { useTheme } from "../../../../contexts/ThemeContext";
 import { useAgentStore } from "../../../../stores/agentStore";
 
 const CHANNELS_WITH_ACCESS_CONTROL: ChannelKey[] = [
@@ -111,7 +109,6 @@ export function ChannelDrawer({
   onSubmit,
 }: ChannelDrawerProps) {
   const { t, i18n } = useTranslation();
-  const { isDark } = useTheme();
   const { selectedAgent, agents } = useAgentStore();
   const currentAgent = agents.find((a) => a.id === selectedAgent);
   const defaultMediaDir = currentAgent?.workspace_dir
@@ -120,96 +117,6 @@ export function ChannelDrawer({
   const currentLang = i18n.language?.startsWith("zh") ? "zh" : "en";
   const label = activeKey ? getChannelLabel(activeKey, t) : activeLabel;
   const { message } = useAppMessage();
-
-  // WeChat QR code hook
-  const wechatQrcode = useChannelQrcode({
-    channel: "wechat",
-    successStatus: "confirmed",
-    successCredentialKey: "bot_token",
-    pollInterval: 2000,
-    onSuccess: useCallback(
-      (credentials: Record<string, string>) => {
-        form.setFieldsValue({ bot_token: credentials.bot_token });
-        message.success(t("channels.wechatLoginSuccess"));
-      },
-      [form, message, t],
-    ),
-    onError: useCallback(
-      (type: "fetch" | "expired") => {
-        if (type === "expired") {
-          message.warning(t("channels.wechatQrcodeExpired"));
-        } else {
-          message.error(t("channels.wechatQrcodeFailed"));
-        }
-      },
-      [message, t],
-    ),
-  });
-
-  // DingTalk QR code hook
-  const dingtalkQrcode = useChannelQrcode({
-    channel: "dingtalk",
-    successStatus: "success",
-    successCredentialKey: "client_id",
-    pollInterval: 5000,
-    onSuccess: useCallback(
-      (credentials: Record<string, string>) => {
-        form.setFieldsValue({
-          client_id: credentials.client_id,
-          client_secret: credentials.client_secret,
-        });
-        message.success(t("channels.dingtalkAuthSuccess"));
-      },
-      [form, message, t],
-    ),
-    onExpired: useCallback(() => {
-      message.warning(t("channels.dingtalkQrcodeExpired"));
-    }, [message, t]),
-    onError: useCallback(
-      (type: "fetch" | "expired") => {
-        if (type === "expired") {
-          message.warning(t("channels.dingtalkQrcodeExpired"));
-        } else {
-          message.error(t("channels.dingtalkQrcodeFailed"));
-        }
-      },
-      [message, t],
-    ),
-  });
-
-  // WeCom QR code hook
-  const wecomQrcode = useChannelQrcode({
-    channel: "wecom",
-    successStatus: "success",
-    successCredentialKey: "bot_id",
-    pollInterval: 3000,
-    onSuccess: useCallback(
-      (credentials: Record<string, string>) => {
-        form.setFieldsValue({
-          bot_id: credentials.bot_id,
-          secret: credentials.secret,
-        });
-        message.success(t("channels.wecomAuthSuccess"));
-      },
-      [form, message, t],
-    ),
-    onError: useCallback(
-      (_type: "fetch" | "expired") => {
-        message.error(t("channels.wecomQrcodeFailed"));
-      },
-      [message, t],
-    ),
-  });
-
-  // Stop all QR code polling when drawer closes
-  useEffect(() => {
-    if (!open) {
-      wechatQrcode.reset();
-      dingtalkQrcode.reset();
-      wecomQrcode.reset();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [open]);
 
   // ── Access control fields (shared across multiple channels) ──────────────
 
@@ -355,41 +262,30 @@ export function ChannelDrawer({
                 style={{ marginBottom: 16 }}
               />
             </ConfigProvider>
-            <Form.Item label={t("channels.dingtalkScanAuth")}>
-              <Button
-                type="primary"
-                block
-                loading={dingtalkQrcode.loading}
-                onClick={dingtalkQrcode.fetchQrcode}
-              >
-                {t("channels.dingtalkGetQrcode")}
-              </Button>
-              {dingtalkQrcode.loading && (
-                <div style={{ textAlign: "center", marginTop: 12 }}>
-                  <Spin />
-                </div>
-              )}
-              {dingtalkQrcode.qrcodeImg && !dingtalkQrcode.loading && (
-                <div style={{ textAlign: "center", marginTop: 12 }}>
-                  <img
-                    src={`data:image/png;base64,${dingtalkQrcode.qrcodeImg}`}
-                    alt="DingTalk QR Code"
-                    style={{ width: 200, height: 200 }}
-                  />
-                  <div
-                    style={{
-                      marginTop: 8,
-                      fontSize: 12,
-                      color: isDark
-                        ? "rgba(255,255,255,0.45)"
-                        : "rgba(0,0,0,0.45)",
-                    }}
-                  >
-                    {t("channels.dingtalkScanHint")}
-                  </div>
-                </div>
-              )}
-            </Form.Item>
+            <QrcodeAuthBlock
+              label={t("channels.dingtalkScanAuth")}
+              buttonText={t("channels.dingtalkGetQrcode")}
+              imageAlt="DingTalk QR Code"
+              hintText={t("channels.dingtalkScanHint")}
+              channel="dingtalk"
+              successStatus="success"
+              successCredentialKey="client_id"
+              pollInterval={5000}
+              onSuccess={(credentials) => {
+                form.setFieldsValue({
+                  client_id: credentials.client_id,
+                  client_secret: credentials.client_secret,
+                });
+                message.success(t("channels.dingtalkAuthSuccess"));
+              }}
+              onError={(type) => {
+                if (type === "expired") {
+                  message.warning(t("channels.dingtalkQrcodeExpired"));
+                } else {
+                  message.error(t("channels.dingtalkQrcodeFailed"));
+                }
+              }}
+            />
             <Form.Item
               name="client_id"
               label="Client ID"
@@ -944,41 +840,26 @@ export function ChannelDrawer({
                 style={{ marginBottom: 16 }}
               />
             </ConfigProvider>
-            <Form.Item label={t("channels.wecomScanAuth")}>
-              <Button
-                type="primary"
-                block
-                loading={wecomQrcode.loading}
-                onClick={wecomQrcode.fetchQrcode}
-              >
-                {t("channels.loginWeCom")}
-              </Button>
-              {wecomQrcode.loading && (
-                <div style={{ textAlign: "center", marginTop: 12 }}>
-                  <Spin />
-                </div>
-              )}
-              {wecomQrcode.qrcodeImg && !wecomQrcode.loading && (
-                <div style={{ textAlign: "center", marginTop: 12 }}>
-                  <img
-                    src={`data:image/png;base64,${wecomQrcode.qrcodeImg}`}
-                    alt="WeCom QR Code"
-                    style={{ width: 200, height: 200 }}
-                  />
-                  <div
-                    style={{
-                      marginTop: 8,
-                      fontSize: 12,
-                      color: isDark
-                        ? "rgba(255,255,255,0.45)"
-                        : "rgba(0,0,0,0.45)",
-                    }}
-                  >
-                    {t("channels.wecomAuthHint")}
-                  </div>
-                </div>
-              )}
-            </Form.Item>
+            <QrcodeAuthBlock
+              label={t("channels.wecomScanAuth")}
+              buttonText={t("channels.loginWeCom")}
+              imageAlt="WeCom QR Code"
+              hintText={t("channels.wecomAuthHint")}
+              channel="wecom"
+              successStatus="success"
+              successCredentialKey="bot_id"
+              pollInterval={3000}
+              onSuccess={(credentials) => {
+                form.setFieldsValue({
+                  bot_id: credentials.bot_id,
+                  secret: credentials.secret,
+                });
+                message.success(t("channels.wecomAuthSuccess"));
+              }}
+              onError={() => {
+                message.error(t("channels.wecomQrcodeFailed"));
+              }}
+            />
             <Form.Item
               name="bot_id"
               label="Bot ID"
@@ -1069,41 +950,27 @@ export function ChannelDrawer({
                 style={{ marginBottom: 16 }}
               />
             </ConfigProvider>
-            <Form.Item label={t("channels.wechatScanLogin")}>
-              <Button
-                type="primary"
-                block
-                loading={wechatQrcode.loading}
-                onClick={wechatQrcode.fetchQrcode}
-              >
-                {t("channels.wechatGetQrcode")}
-              </Button>
-              {wechatQrcode.loading && (
-                <div style={{ textAlign: "center", marginTop: 12 }}>
-                  <Spin />
-                </div>
-              )}
-              {wechatQrcode.qrcodeImg && !wechatQrcode.loading && (
-                <div style={{ textAlign: "center", marginTop: 12 }}>
-                  <img
-                    src={`data:image/png;base64,${wechatQrcode.qrcodeImg}`}
-                    alt="WeChat QR Code"
-                    style={{ width: 200, height: 200 }}
-                  />
-                  <div
-                    style={{
-                      marginTop: 8,
-                      fontSize: 12,
-                      color: isDark
-                        ? "rgba(255,255,255,0.45)"
-                        : "rgba(0,0,0,0.45)",
-                    }}
-                  >
-                    {t("channels.wechatScanHint")}
-                  </div>
-                </div>
-              )}
-            </Form.Item>
+            <QrcodeAuthBlock
+              label={t("channels.wechatScanLogin")}
+              buttonText={t("channels.wechatGetQrcode")}
+              imageAlt="WeChat QR Code"
+              hintText={t("channels.wechatScanHint")}
+              channel="wechat"
+              successStatus="confirmed"
+              successCredentialKey="bot_token"
+              pollInterval={2000}
+              onSuccess={(credentials) => {
+                form.setFieldsValue({ bot_token: credentials.bot_token });
+                message.success(t("channels.wechatLoginSuccess"));
+              }}
+              onError={(type) => {
+                if (type === "expired") {
+                  message.warning(t("channels.wechatQrcodeExpired"));
+                } else {
+                  message.error(t("channels.wechatQrcodeFailed"));
+                }
+              }}
+            />
             <Form.Item
               name="bot_token"
               label={t("channels.wechatBotToken")}

cofly-io/xClaw

console/src/pages/Control/Channels/components/ChannelDrawer.tsx
@@ -8,15 +8,13 @@ import {
   Select,
 } from "@agentscope-ai/design";
 import { useAppMessage } from "../../../../hooks/useAppMessage";
-import { Alert, ConfigProvider, Spin } from "antd";
+import { Alert, ConfigProvider } from "antd";
 import { LinkOutlined } from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
 import type { FormInstance } from "antd";
-import { useCallback, useEffect } from "react";
 import { getChannelLabel, type ChannelKey } from "./constants";
-import { useChannelQrcode } from "./useChannelQrcode";
+import { QrcodeAuthBlock } from "./QrcodeAuthBlock";
 import styles from "../index.module.less";
-import { useTheme } from "../../../../contexts/ThemeContext";
 import { useAgentStore } from "../../../../stores/agentStore";
 
 const CHANNELS_WITH_ACCESS_CONTROL: ChannelKey[] = [
@@ -102,7 +100,6 @@ export function ChannelDrawer({
   onSubmit,
 }: ChannelDrawerProps) {
   const { t, i18n } = useTranslation();
-  const { isDark } = useTheme();
   const { selectedAgent, agents } = useAgentStore();
   const currentAgent = agents.find((a) => a.id === selectedAgent);
   const defaultMediaDir = currentAgent?.workspace_dir
@@ -111,96 +108,6 @@ export function ChannelDrawer({
   const currentLang = i18n.language?.startsWith("zh") ? "zh" : "en";
   const label = activeKey ? getChannelLabel(activeKey, t) : activeLabel;
   const { message } = useAppMessage();
-
-  // WeChat QR code hook
-  const weixinQrcode = useChannelQrcode({
-    channel: "weixin",
-    successStatus: "confirmed",
-    successCredentialKey: "bot_token",
-    pollInterval: 2000,
-    onSuccess: useCallback(
-      (credentials: Record<string, string>) => {
-        form.setFieldsValue({ bot_token: credentials.bot_token });
-        message.success(t("channels.weixinLoginSuccess"));
-      },
-      [form, message, t],
-    ),
-    onError: useCallback(
-      (type: "fetch" | "expired") => {
-        if (type === "expired") {
-          message.warning(t("channels.weixinQrcodeExpired"));
-        } else {
-          message.error(t("channels.weixinQrcodeFailed"));
-        }
-      },
-      [message, t],
-    ),
-  });
-
-  // DingTalk QR code hook
-  const dingtalkQrcode = useChannelQrcode({
-    channel: "dingtalk",
-    successStatus: "success",
-    successCredentialKey: "client_id",
-    pollInterval: 5000,
-    onSuccess: useCallback(
-      (credentials: Record<string, string>) => {
-        form.setFieldsValue({
-          client_id: credentials.client_id,
-          client_secret: credentials.client_secret,
-        });
-        message.success(t("channels.dingtalkAuthSuccess"));
-      },
-      [form, message, t],
-    ),
-    onExpired: useCallback(() => {
-      message.warning(t("channels.dingtalkQrcodeExpired"));
-    }, [message, t]),
-    onError: useCallback(
-      (type: "fetch" | "expired") => {
-        if (type === "expired") {
-          message.warning(t("channels.dingtalkQrcodeExpired"));
-        } else {
-          message.error(t("channels.dingtalkQrcodeFailed"));
-        }
-      },
-      [message, t],
-    ),
-  });
-
-  // WeCom QR code hook
-  const wecomQrcode = useChannelQrcode({
-    channel: "wecom",
-    successStatus: "success",
-    successCredentialKey: "bot_id",
-    pollInterval: 3000,
-    onSuccess: useCallback(
-      (credentials: Record<string, string>) => {
-        form.setFieldsValue({
-          bot_id: credentials.bot_id,
-          secret: credentials.secret,
-        });
-        message.success(t("channels.wecomAuthSuccess"));
-      },
-      [form, message, t],
-    ),
-    onError: useCallback(
-      (_type: "fetch" | "expired") => {
-        message.error(t("channels.wecomQrcodeFailed"));
-      },
-      [message, t],
-    ),
-  });
-
-  // Stop all QR code polling when drawer closes
-  useEffect(() => {
-    if (!open) {
-      wechatQrcode.reset();
-      dingtalkQrcode.reset();
-      wecomQrcode.reset();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [open]);
 
   // ── Access control fields (shared across multiple channels) ──────────────
 
@@ -346,41 +253,31 @@ export function ChannelDrawer({
                 style={{ marginBottom: 16 }}
               />
             </ConfigProvider>
-            <Form.Item label={t("channels.dingtalkScanAuth")}>
-              <Button
-                type="primary"
-                block
-                loading={dingtalkQrcode.loading}
-                onClick={dingtalkQrcode.fetchQrcode}
-              >
-                {t("channels.dingtalkGetQrcode")}
-              </Button>
-              {dingtalkQrcode.loading && (
-                <div style={{ textAlign: "center", marginTop: 12 }}>
-                  <Spin />
-                </div>
-              )}
-              {dingtalkQrcode.qrcodeImg && !dingtalkQrcode.loading && (
-                <div style={{ textAlign: "center", marginTop: 12 }}>
-                  <img
-                    src={`data:image/png;base64,${dingtalkQrcode.qrcodeImg}`}
-                    alt="DingTalk QR Code"
-                    style={{ width: 200, height: 200 }}
-                  />
-                  <div
-                    style={{
-                      marginTop: 8,
-                      fontSize: 12,
-                      color: isDark
-                        ? "rgba(255,255,255,0.45)"
-                        : "rgba(0,0,0,0.45)",
-                    }}
-                  >
-                    {t("channels.dingtalkScanHint")}
-                  </div>
-                </div>
-              )}
-            </Form.Item>
+            <QrcodeAuthBlock
+              drawerOpen={open}
+              label={t("channels.dingtalkScanAuth")}
+              buttonText={t("channels.dingtalkGetQrcode")}
+              imageAlt="DingTalk QR Code"
+              hintText={t("channels.dingtalkScanHint")}
+              channel="dingtalk"
+              successStatus="success"
+              successCredentialKey="client_id"
+              pollInterval={5000}
+              onSuccess={(credentials) => {
+                form.setFieldsValue({
+                  client_id: credentials.client_id,
+                  client_secret: credentials.client_secret,
+                });
+                message.success(t("channels.dingtalkAuthSuccess"));
+              }}
+              onError={(type) => {
+                if (type === "expired") {
+                  message.warning(t("channels.dingtalkQrcodeExpired"));
+                } else {
+                  message.error(t("channels.dingtalkQrcodeFailed"));
+                }
+              }}
+            />
             <Form.Item
               name="client_id"
               label="Client ID"
@@ -777,41 +674,27 @@ export function ChannelDrawer({
                 style={{ marginBottom: 16 }}
               />
             </ConfigProvider>
-            <Form.Item label={t("channels.wecomScanAuth")}>
-              <Button
-                type="primary"
-                block
-                loading={wecomQrcode.loading}
-                onClick={wecomQrcode.fetchQrcode}
-              >
-                {t("channels.loginWeCom")}
-              </Button>
-              {wecomQrcode.loading && (
-                <div style={{ textAlign: "center", marginTop: 12 }}>
-                  <Spin />
-                </div>
-              )}
-              {wecomQrcode.qrcodeImg && !wecomQrcode.loading && (
-                <div style={{ textAlign: "center", marginTop: 12 }}>
-                  <img
-                    src={`data:image/png;base64,${wecomQrcode.qrcodeImg}`}
-                    alt="WeCom QR Code"
-                    style={{ width: 200, height: 200 }}
-                  />
-                  <div
-                    style={{
-                      marginTop: 8,
-                      fontSize: 12,
-                      color: isDark
-                        ? "rgba(255,255,255,0.45)"
-                        : "rgba(0,0,0,0.45)",
-                    }}
-                  >
-                    {t("channels.wecomAuthHint")}
-                  </div>
-                </div>
-              )}
-            </Form.Item>
+            <QrcodeAuthBlock
+              drawerOpen={open}
+              label={t("channels.wecomScanAuth")}
+              buttonText={t("channels.loginWeCom")}
+              imageAlt="WeCom QR Code"
+              hintText={t("channels.wecomAuthHint")}
+              channel="wecom"
+              successStatus="success"
+              successCredentialKey="bot_id"
+              pollInterval={3000}
+              onSuccess={(credentials) => {
+                form.setFieldsValue({
+                  bot_id: credentials.bot_id,
+                  secret: credentials.secret,
+                });
+                message.success(t("channels.wecomAuthSuccess"));
+              }}
+              onError={() => {
+                message.error(t("channels.wecomQrcodeFailed"));
+              }}
+            />
             <Form.Item
               name="bot_id"
               label="Bot ID"
@@ -902,41 +785,28 @@ export function ChannelDrawer({
                 style={{ marginBottom: 16 }}
               />
             </ConfigProvider>
-            <Form.Item label={t("channels.weixinScanLogin")}>
-              <Button
-                type="primary"
-                block
-                loading={weixinQrcode.loading}
-                onClick={weixinQrcode.fetchQrcode}
-              >
-                {t("channels.weixinGetQrcode")}
-              </Button>
-              {weixinQrcode.loading && (
-                <div style={{ textAlign: "center", marginTop: 12 }}>
-                  <Spin />
-                </div>
-              )}
-              {weixinQrcode.qrcodeImg && !weixinQrcode.loading && (
-                <div style={{ textAlign: "center", marginTop: 12 }}>
-                  <img
-                    src={`data:image/png;base64,${weixinQrcode.qrcodeImg}`}
-                    alt="WeChat QR Code"
-                    style={{ width: 200, height: 200 }}
-                  />
-                  <div
-                    style={{
-                      marginTop: 8,
-                      fontSize: 12,
-                      color: isDark
-                        ? "rgba(255,255,255,0.45)"
-                        : "rgba(0,0,0,0.45)",
-                    }}
-                  >
-                    {t("channels.weixinScanHint")}
-                  </div>
-                </div>
-              )}
-            </Form.Item>
+            <QrcodeAuthBlock
+              drawerOpen={open}
+              label={t("channels.weixinScanLogin")}
+              buttonText={t("channels.weixinGetQrcode")}
+              imageAlt="WeChat QR Code"
+              hintText={t("channels.weixinScanHint")}
+              channel="weixin"
+              successStatus="confirmed"
+              successCredentialKey="bot_token"
+              pollInterval={2000}
+              onSuccess={(credentials) => {
+                form.setFieldsValue({ bot_token: credentials.bot_token });
+                message.success(t("channels.weixinLoginSuccess"));
+              }}
+              onError={(type) => {
+                if (type === "expired") {
+                  message.warning(t("channels.weixinQrcodeExpired"));
+                } else {
+                  message.error(t("channels.weixinQrcodeFailed"));
+                }
+              }}
+            />
             <Form.Item
               name="bot_token"
               label={t("channels.weixinBotToken")}