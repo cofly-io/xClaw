devjotaduo/orbe

feat(plugins): add Qwen-Image and Wan 2.7 plugins (#4248)

src/qwenpaw/plugins/api.py
@@ -1,10 +1,48 @@
 # -*- coding: utf-8 -*-
 """Plugin API for plugin developers."""
 
-from typing import Any, Callable, Dict, Type
 import logging
+from typing import Any, Callable, Dict, Optional, Type
 
 logger = logging.getLogger(__name__)
+
+
+def get_tool_config(tool_name: str) -> Optional[Dict[str, Any]]:
+    """Get tool configuration for the currently active agent.
+
+    Convenience helper for tool plugin developers. Call this inside
+    tool functions to retrieve user-configured values (api_key,
+    endpoint, timeout, etc.) without needing a PluginApi reference.
+
+    Args:
+        tool_name: The registered name of the tool function.
+
+    Returns:
+        Configuration dict or None if the tool is not configured.
+
+    Example:
+        >>> from qwenpaw.plugins import get_tool_config
+        >>> config = get_tool_config("my_tool")
+        >>> if not config:
+        ...     return ToolResponse(...)  # not configured
+        >>> api_key = config.get("api_key")
+    """
+    try:
+        from .registry import PluginRegistry
+        from ..app.agent_context import get_current_agent_id
+
+        agent_id = get_current_agent_id()
+        if not agent_id:
+            logger.warning(
+                "get_tool_config: no current agent ID found",
+            )
+            return None
+
+        registry = PluginRegistry()
+        return registry.get_tool_config(tool_name, agent_id)
+    except Exception as e:
+        logger.error(f"get_tool_config failed for {tool_name}: {e}")
+        return None
 
 
 class PluginApi:
@@ -214,3 +252,118 @@ class PluginApi:
         """
         if self._registry:
             self._registry.set_tool_config(tool_name, agent_id, config)
+
+    def register_tool(
+        self,
+        tool_name: str,
+        tool_func: Callable,
+        description: str = "",
+        icon: str = "🔧",
+        enabled: bool = False,
+    ) -> None:
+        """Register a tool function into the Agent's toolkit.
+
+        This is the recommended way for tool plugins to register tools.
+        It handles all registration boilerplate:
+        - Adds the function to ``qwenpaw.agents.tools`` module
+        - Appends the name to ``tools.__all__``
+        - Creates a ``BuiltinToolConfig`` entry in the current agent
+          config (disabled by default so the user can opt-in)
+
+        The actual registration is deferred to a startup hook so it
+        runs after the application and agent context are fully
+        initialized.
+
+        Args:
+            tool_name: Unique name for the tool function.
+                Must match the function name used in the agent prompt.
+            tool_func: The async (or sync) tool callable to register.
+            description: Human-readable description shown in the UI.
+            icon: Display icon (emoji string). Default: "🔧".
+            enabled: Whether the tool is enabled by default. The
+                recommended value is False so the user explicitly
+                enables the tool. Default: False.
+
+        Example:
+            >>> from .tool import my_tool_func
+            >>> def register(self, api: PluginApi):
+            ...     api.register_tool(
+            ...         tool_name="my_tool",
+            ...         tool_func=my_tool_func,
+            ...         description="Does something useful",
+            ...         icon="🔧",
+            ...     )
+        """
+
+        def _startup_register():
+            try:
+                import qwenpaw.agents.tools as tools_module
+
+                setattr(tools_module, tool_name, tool_func)
+                if tool_name not in tools_module.__all__:
+                    tools_module.__all__.append(tool_name)
+                logger.info(
+                    f"Registered tool function '{tool_name}' "
+                    f"to tools module",
+                )
+
+                from ..config.config import (
+                    BuiltinToolConfig,
+                    load_agent_config,
+                    save_agent_config,
+                )
+                from ..app.agent_context import get_current_agent_id
+
+                agent_id = get_current_agent_id()
+                if not agent_id:
+                    logger.warning(
+                        f"No current agent ID; tool '{tool_name}' "
+                        f"will be available after restart",
+                    )
+                    return
+
+                agent_config = load_agent_config(agent_id)
+
+                if not agent_config.tools:
+                    from ..config.config import ToolsConfig
+
+                    agent_config.tools = ToolsConfig()
+
+                if tool_name not in agent_config.tools.builtin_tools:
+                    agent_config.tools.builtin_tools[
+                        tool_name
+                    ] = BuiltinToolConfig(
+                        name=tool_name,
+                        enabled=enabled,
+                        description=description,
+                        display_to_user=True,
+                        async_execution=False,
+                        icon=icon,
+                    )
+                    logger.info(
+                        f"Added tool '{tool_name}' to agent "
+                        f"'{agent_id}' config (enabled={enabled})",
+                    )
+                else:
+                    logger.info(
+                        f"Tool '{tool_name}' already in agent "
+                        f"'{agent_id}' config, skipping",
+                    )
+
+                save_agent_config(agent_id, agent_config)
+
+            except Exception as exc:
+                logger.error(
+                    f"Failed to register tool '{tool_name}': {exc}",
+                    exc_info=True,
+                )
+
+        self.register_startup_hook(
+            hook_name=f"register_tool_{self.plugin_id}_{tool_name}",
+            callback=_startup_register,
+            priority=50,
+        )
+        logger.info(
+            f"Plugin '{self.plugin_id}' scheduled tool "
+            f"'{tool_name}' for registration on startup",
+        )

cofly-io/xClaw

Co-authored-by: Cursor <cursoragent@cursor.com>

src/xclaw/plugins/api.py
@@ -1,10 +1,48 @@
 # -*- coding: utf-8 -*-
 """Plugin API for plugin developers."""
 
-from typing import Any, Callable, Dict, Type
 import logging
+from typing import Any, Callable, Dict, Optional, Type
 
 logger = logging.getLogger(__name__)
+
+
+def get_tool_config(tool_name: str) -> Optional[Dict[str, Any]]:
+    """Get tool configuration for the currently active agent.
+
+    Convenience helper for tool plugin developers. Call this inside
+    tool functions to retrieve user-configured values (api_key,
+    endpoint, timeout, etc.) without needing a PluginApi reference.
+
+    Args:
+        tool_name: The registered name of the tool function.
+
+    Returns:
+        Configuration dict or None if the tool is not configured.
+
+    Example:
+        >>> from xclaw.plugins import get_tool_config
+        >>> config = get_tool_config("my_tool")
+        >>> if not config:
+        ...     return ToolResponse(...)  # not configured
+        >>> api_key = config.get("api_key")
+    """
+    try:
+        from .registry import PluginRegistry
+        from ..app.agent_context import get_current_agent_id
+
+        agent_id = get_current_agent_id()
+        if not agent_id:
+            logger.warning(
+                "get_tool_config: no current agent ID found",
+            )
+            return None
+
+        registry = PluginRegistry()
+        return registry.get_tool_config(tool_name, agent_id)
+    except Exception as e:
+        logger.error(f"get_tool_config failed for {tool_name}: {e}")
+        return None
 
 
 class PluginApi:
@@ -183,3 +221,149 @@ class PluginApi:
         if self._registry:
             return self._registry.get_runtime_helpers()
         return None
+
+    def get_tool_config(self, tool_name: str, agent_id: str) -> dict:
+        """Get tool configuration from registry.
+
+        Args:
+            tool_name: Tool function name
+            agent_id: Agent identifier
+
+        Returns:
+            Tool configuration dictionary (empty if not configured)
+        """
+        if self._registry:
+            config = self._registry.get_tool_config(tool_name, agent_id)
+            return config if config else {}
+        return {}
+
+    def set_tool_config(
+        self,
+        tool_name: str,
+        agent_id: str,
+        config: dict,
+    ) -> None:
+        """Save tool configuration to registry.
+
+        Args:
+            tool_name: Tool function name
+            agent_id: Agent identifier
+            config: Configuration dictionary
+        """
+        if self._registry:
+            self._registry.set_tool_config(tool_name, agent_id, config)
+
+    def register_tool(
+        self,
+        tool_name: str,
+        tool_func: Callable,
+        description: str = "",
+        icon: str = "🔧",
+        enabled: bool = False,
+    ) -> None:
+        """Register a tool function into the Agent's toolkit.
+
+        This is the recommended way for tool plugins to register tools.
+        It handles all registration boilerplate:
+        - Adds the function to ``xclaw.agents.tools`` module
+        - Appends the name to ``tools.__all__``
+        - Creates a ``BuiltinToolConfig`` entry in the current agent
+          config (disabled by default so the user can opt-in)
+
+        The actual registration is deferred to a startup hook so it
+        runs after the application and agent context are fully
+        initialized.
+
+        Args:
+            tool_name: Unique name for the tool function.
+                Must match the function name used in the agent prompt.
+            tool_func: The async (or sync) tool callable to register.
+            description: Human-readable description shown in the UI.
+            icon: Display icon (emoji string). Default: "🔧".
+            enabled: Whether the tool is enabled by default. The
+                recommended value is False so the user explicitly
+                enables the tool. Default: False.
+
+        Example:
+            >>> from .tool import my_tool_func
+            >>> def register(self, api: PluginApi):
+            ...     api.register_tool(
+            ...         tool_name="my_tool",
+            ...         tool_func=my_tool_func,
+            ...         description="Does something useful",
+            ...         icon="🔧",
+            ...     )
+        """
+
+        def _startup_register():
+            try:
+                import xclaw.agents.tools as tools_module
+
+                setattr(tools_module, tool_name, tool_func)
+                if tool_name not in tools_module.__all__:
+                    tools_module.__all__.append(tool_name)
+                logger.info(
+                    f"Registered tool function '{tool_name}' "
+                    f"to tools module",
+                )
+
+                from ..config.config import (
+                    BuiltinToolConfig,
+                    load_agent_config,
+                    save_agent_config,
+                )
+                from ..app.agent_context import get_current_agent_id
+
+                agent_id = get_current_agent_id()
+                if not agent_id:
+                    logger.warning(
+                        f"No current agent ID; tool '{tool_name}' "
+                        f"will be available after restart",
+                    )
+                    return
+
+                agent_config = load_agent_config(agent_id)
+
+                if not agent_config.tools:
+                    from ..config.config import ToolsConfig
+
+                    agent_config.tools = ToolsConfig()
+
+                if tool_name not in agent_config.tools.builtin_tools:
+                    agent_config.tools.builtin_tools[
+                        tool_name
+                    ] = BuiltinToolConfig(
+                        name=tool_name,
+                        enabled=enabled,
+                        description=description,
+                        display_to_user=True,
+                        async_execution=False,
+                        icon=icon,
+                    )
+                    logger.info(
+                        f"Added tool '{tool_name}' to agent "
+                        f"'{agent_id}' config (enabled={enabled})",
+                    )
+                else:
+                    logger.info(
+                        f"Tool '{tool_name}' already in agent "
+                        f"'{agent_id}' config, skipping",
+                    )
+
+                save_agent_config(agent_id, agent_config)
+
+            except Exception as exc:
+                logger.error(
+                    f"Failed to register tool '{tool_name}': {exc}",
+                    exc_info=True,
+                )
+
+        self.register_startup_hook(
+            hook_name=f"register_tool_{self.plugin_id}_{tool_name}",
+            callback=_startup_register,
+            priority=50,
+        )
+        logger.info(
+            f"Plugin '{self.plugin_id}' scheduled tool "
+            f"'{tool_name}' for registration on startup",
+        )